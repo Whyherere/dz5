1bdd51962f3d058c53cb5c5663d929270d6e7d6c
Whyhere <148620619+Whyherere@users.noreply.github.com>
2024-04-19 15:44:42 +0300

    Add files via upload

---
+++ cikl/php.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+/* 2 $string = "Hello World!";
+$str = strrev($string);
+echo $str; */
+/*4 $string = "Hello World !";
+$Str = str_replace(' ', '_', $string);
+echo $Str; */
+$string = "Hello World !";
+$words = explode(' ', $string); 
+    ?>
+    
+</body>
+</html>
